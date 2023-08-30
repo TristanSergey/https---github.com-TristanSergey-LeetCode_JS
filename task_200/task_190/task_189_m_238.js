4d474e77c9fa4c8d15e3a66b7a0d1319ab8f3d27
Sergey Tristan <svt@mail.ru>
2023-08-30 18:13:19 +0300

    30082023

--- task_200/task_190/task_189_m_238.js
+++ task_200/task_190/task_189_m_238.js
@@ -4,23 +4,76 @@
 // Вы должны написать алгоритм, который работает за время O(n) и не использует операцию деления.
 
 // 238
-nums = [1, 2, 3, 4]
-
+nums = [2, 3, 4, 5]
+nums = [-1, 1, 0, -3, 3]
 var productExceptSelf = function (nums) {
     const length = nums.length - 1;
-    const left = [...nums];
-    const right = [...nums];
+    const answer = [];
+    answer[0] = 1;
 
-    for (let i = 1; i < length; i++) left[i + 1] *= left[i] * 0;
-    for (let i = length; i > 0; i--) right[i - 1] *= right[i] * 0;
+    for (let i = 1; i <= length; i++) answer[i] = nums[i - 1] * answer[i - 1];
 
-
-    console.log(right)
-
-
-
-    console.log(left)
-
+    let right = 1;
+    for (let i = length; i >= 0; i--) {
+        answer[i] *= right;
+        right *= nums[i]
+    }
+    return answer;
 };
 
 console.log(productExceptSelf(nums))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//var productExceptSelf = function (nums) {
+//     const length = nums.length - 1;
+//     const left = [...nums];
+//     const right = [...nums];
+//     const answer = [];
+
+//     left.unshift(1)
+//     right.push(1)
+//     console.log(left)
+//     for (let i = 2; i <= length; i++) {
+
+
+//         left[i] = left[i - 1] * left[i];
+//         console.log(left)
+//     }
+
+//     console.log(right)
+//     for (let i = length; i > 0; i--) {
+//         console.log(right)
+//         right[i - 1] = right[i] * right[i - 1];
+//     }
+
+//     right.shift();
+//     left.pop();
+//     console.log(right)
+//     console.log(left)
+
+//     for (let i = 0; i <= length; i++) answer.push(left[i] * right[i])
+
+//     return answer;
+
+// };
