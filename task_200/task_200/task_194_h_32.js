6ffaa031b628ee38be76ce42ed82ed5fa020fa89
Sergey Tristan <svt@mail.ru>
2023-08-31 22:55:32 +0300

    31082023

--- task_200/task_200/task_194_h_32.js
+++ task_200/task_200/task_194_h_32.js
@@ -3,43 +3,50 @@
 // 32
 
 s = ")()())";
-s = ')(()((())())()))()'
+//s = ')(()((())())()))()'
 // s = ')(()())))((())())()()()'
 //s = ')())()())))()()()'
 //s = "(()"
-s = "()(()"
+//s = "()(((()())"
 var longestValidParentheses = function (s) {
 
     const stack = [];
+    stack[0] = -1;
     let answer = 0;
     let currentCount = 0;
 
     for (let i = 0; i < s.length; i++) {
         console.log(' --------  ')
+
+
         if (s[i] === '(') {
-            stack.push(1);
-            currentCount++;
+            stack.push(i);
         }
 
         if (s[i] === ')') {
             if (stack.length === 0) {
-                currentCount = 0;
+                stack.push(i);
             } else {
-                stack.pop(0);
-                currentCount++;
+                stack.pop();
+                if (stack.length === 0) {
+                    stack.push(i);
+                } else {
+                    currentCount = i - stack[stack.length - 1];
+                    answer = Math.max(answer, currentCount);
+                }
             }
         }
+        //if (stack.length === 0) 
+
         answer = Math.max(answer, currentCount);
-        // if (s[i] === ')') {
-        //     stack.pop(1);
-        //     currentCount++;
-        // }
 
         console.log(stack)
-        console.log(currentCount + ' - currentCount- ' + answer + ' - answer - ' + stack.length + ' - stack.length - ')
+
+        console.log(currentCount + ' - currentCount- ' + answer + ' - answer - ');
+        console.log(stack.length + ' - stack.length - ')
     }
 
-    return answer - stack.length;
+    return answer;
 };
 
 console.log(longestValidParentheses(s))
---
+++ task_200/task_200/task_199_e_344.js
@@ -0,0 +1,20 @@
+﻿// Напишите функцию, которая переворачивает строку.Входная строка задается как массив символов s.
+// Вы должны сделать это, изменив входной массив на месте с дополнительной памятью O(1).
+// 344
+
+s = ["h", "e", "l", "l", "o"]
+
+var reverseString = function (s) {
+    const length = s.length - 1;
+    for (let i = 0; i <= length; i++) {
+
+        const char = s.shift();
+        s.push(char);
+        console.log(char)
+        console.log(s)
+    }
+
+
+};
+
+console.log(reverseString(s))
