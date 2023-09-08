34be7dd330976a03fa33a7963d4f09945c912cfe
TristanSergey <tristan.sergey@gmail.com>
2023-09-08 11:52:30 +0300

    08092023 filename change

---
+++ task_100/Task20/task_12_m_287.js
@@ -0,0 +1,95 @@
+﻿// Дан массив целых чисел nums, содержащий n + 1 целых чисел,
+// где каждое целое число находится в диапазоне[1, n] включительно.
+// В nums есть только одно повторяющееся число, верните
+// это повторяющееся число.
+// Вы должны решить проблему, не изменяя массив nums и
+// используя только постоянное дополнительное пространство.
+// 287
+
+//let nums = [3, 1, 3, 4, 2];
+//let nums = [1, 3, 4, 2, 2];
+//let nums = [1, 3, 4, 2, 5, 6, 2];
+
+let nums = [1, 3, 4, 1, 7, 6, 2, 5];
+
+function findDuplicate(nums) {
+    // if (nums.length < 2) {
+    //     return false;
+    // }
+    // let set = new Set();
+    // //map.set = nums;
+    // for (let i = 0; i < nums.length; i++) {
+    //     if (set.has(nums[i])) {
+    //         return nums[i]
+    //     } else {
+    //         set.add(nums[i]);
+    //     }
+    // }
+
+    // let slow = 0, fast = 0;
+    // while (true) {
+    //     slow = nums[slow]
+    //     console.log(slow + ' -slow- ' + nums[slow] + ' -nums[slow]- ');
+    //     fast = nums[nums[fast]]
+    //     console.log(fast + ' -fast- ' + nums[fast] + ' -nums[fast]- ');
+    //     if (slow === fast) break;
+    // }
+
+    // console.log(fast + ' -fast- ' + slow + ' -slow- ');
+    // fast = 0;
+    // while (true) {
+    //     slow = nums[slow]
+    //     fast = nums[fast]
+    //     if (slow === fast) return slow
+    // }
+
+    // if (slow === fast) {
+    //     return slow;
+    // } else {
+    //     return null; // или return -1; или другое подходящее значение
+    // }
+
+    //console.log(map);
+    // let right = nums.length - 1;
+
+    // for (let left = 0; left < nums.length; left++) {
+    //     while (right > left) {
+    //         if (nums[left] != nums[right]) {
+    //             right--;
+    //         } else {
+    //             return nums[left]
+    //         }
+
+    //     }
+    //     right = nums.length - 1;
+    // }
+
+    // for (let i = 0; i < nums.length; i++) {
+    //     for (let j = nums.length; j > i; j--) {
+    //         if (nums[i] == nums[j]) {
+    //             return nums[i];
+    //         }
+    //     }
+    // }
+
+    let slow = 0;
+    let fast = 0;
+
+    while (true) {
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+        if (slow === fast) {
+            break;
+        }
+    }
+    fast = 0;
+    while (true) {
+        slow = nums[slow];
+        fast = nums[fast];
+        if (slow === fast) {
+            return slow;
+        }
+    }
+}
+
+console.log(findDuplicate(nums));
---
+++ task_100/Task30/task_29_m_2618.js
@@ -0,0 +1,29 @@
+﻿// Write a function that checks if a given value is an 
+// instance of a given class or superclass.
+// For this problem, an object is considered an instance 
+// of a given class if that object has access to that class's methods.
+// There are no constraints on the data types that can be 
+// passed to the function. For example, the value or the 
+// class could be undefined.
+//2618
+
+function checkIfInstanceOf(obj, classFunction) {
+
+    if (obj == null || obj == undefined || typeof classFunction != 'function') {
+        return false;
+    }
+
+    let potentialPrototype = Object.getPrototypeOf(obj);
+
+    while (potentialPrototype != null) {
+        if (potentialPrototype === classFunction.prototype) {
+            return true;
+        }
+        potentialPrototype = Object.getPrototypeOf(potentialPrototype);
+    }
+
+    return false;
+
+};
+
+console.log(checkIfInstanceOf(obj, classFunction));
