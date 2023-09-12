55be8e72506b7647c1196ddcf7664d6bf23232f2
TristanSergey <tristan.sergey@gmail.com>
2023-09-12 21:49:08 +0300

    12092023

--- task_300/task_240/task_231_h_41.js
+++ task_300/task_240/task_231_h_41.js
@@ -4,28 +4,72 @@
 
 nums = [1, 2, 0]
 nums = [3, 4, -1, 1]
-//nums = [7, 8, 9, 11, 12]
+nums = [7, 8, 9, 11, 12]
 
 
 var firstMissingPositive = function (nums) {
     const length = nums.length;
-    const hash = new Map();
+    const hash = new Set();
     let minPositive = 1;
     let max = 0;
+    //nums.sort()
+    console.log(nums)
     for (let i = 0; i < length; i++) {
         if (nums[i] > 0) {
-            if (nums[i] > max) max = nums[i];
-            console.log(max + ' - max -')
-            if (!(minPositive < nums[i])) minPositive = max;
-            if (!(minPositive = nums[i])) minPositive += nums[i];
-            if (!(minPositive > nums[i])) max += 1;
-            console.log(minPositive + ' - minPositive -')
+            hash.add(nums[i])
+            console.log(hash)
         }
     }
 
+    for (let i = 0; i < length; i++) {
+        if (hash.has(minPositive)) minPositive++
+    }
     return minPositive;
 
 };
 
 
 console.log(firstMissingPositive(nums))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var firstMissingPositive = function (nums) {
+//     const length = nums.length;
+//     const hash = new Set();
+//     let minPositive = 1;
+//     let max = 0;
+//     nums.sort()
+//     console.log(nums)
+//     for (let i = 0; i < length; i++) {
+//         if (nums[i] > 0) {
+//             hash.add(nums[i])
+//             console.log(hash)
+//         }
+//     }
+
+//     for (let i = 0; i < length; i++) {
+//         if (hash.has(minPositive)) minPositive++
+//     }
+//     return minPositive;
+
+// };
