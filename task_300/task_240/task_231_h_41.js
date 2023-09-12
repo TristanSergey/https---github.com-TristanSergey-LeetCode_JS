d65ec5ff92f0c81e3283fbcc44132cf95ab5d8ec
TristanSergey <tristan.sergey@gmail.com>
2023-09-12 14:52:27 +0300

    12092023

---
+++ task_300/task_240/task_231_h_41.js
@@ -0,0 +1,31 @@
+﻿// Учитывая несортированный целочисленный массив nums, верните наименьшее пропущенное положительное целое число.
+// Вы должны реализовать алгоритм, который работает за время O(n) и использует вспомогательное пространство O(1).
+// 41
+
+nums = [1, 2, 0]
+nums = [3, 4, -1, 1]
+//nums = [7, 8, 9, 11, 12]
+
+
+var firstMissingPositive = function (nums) {
+    const length = nums.length;
+    const hash = new Map();
+    let minPositive = 1;
+    let max = 0;
+    for (let i = 0; i < length; i++) {
+        if (nums[i] > 0) {
+            if (nums[i] > max) max = nums[i];
+            console.log(max + ' - max -')
+            if (!(minPositive < nums[i])) minPositive = max;
+            if (!(minPositive = nums[i])) minPositive += nums[i];
+            if (!(minPositive > nums[i])) max += 1;
+            console.log(minPositive + ' - minPositive -')
+        }
+    }
+
+    return minPositive;
+
+};
+
+
+console.log(firstMissingPositive(nums))
