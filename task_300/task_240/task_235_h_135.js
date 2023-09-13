6bf0540405e5ea1d863e1ff8cacdfea3b305dba0
TristanSergey <tristan.sergey@gmail.com>
2023-09-13 22:27:38 +0300

    13092023

--- task_300/task_240/task_235_h_135.js
+++ task_300/task_240/task_235_h_135.js
@@ -6,6 +6,30 @@
 // Верните минимальное количество конфет, которое вам нужно, чтобы раздать конфеты детям.
 // 135
 
+ratings = [1, 2, 2]
+ratings = [1, 2, 3, 2, 2, 1, 3]
+
 var candy = function (ratings) {
+    const length = ratings.length;
+    const answer = Array.from({ length: length }).fill(1);
+    for (let i = 1; i < length; i++) {
+        if (ratings[i - 1] < ratings[i]) {
+            answer[i] += answer[i - 1]
+        }
+    }
+    //console.log(answer)
+    for (let i = length - 1; i > 0; i--) {
+        //console.log(i)
+        //console.log(ratings[i - 1] + ' - ratings[i - 1] - ' + ratings[i] + ' - ratings[i]- ' + answer[i - 1] + ' - answer[i - 1] -' + answer[i] + ' - answer[i]- ')
+        if (ratings[i - 1] > ratings[i] && answer[i - 1] <= answer[i]) {
+            console.log('++++++ ')
+            answer[i - 1] = answer[i] + 1;
+        }
+
+        // console.log(answer)
+    }
+    return answer.reduce((acc, item) => acc += item, 0)
 
 };
+
+console.log(candy(ratings))
---
+++ task_300/task_240/task_239_e_872.js
@@ -0,0 +1,64 @@
+﻿// Рассмотрим все листья двоичного дерева, слева направо, значения этих листьев образуют 
+// последовательность значений листьев.
+// Например, в приведенном выше дереве последовательность значений листьев равна(6, 7, 4, 9, 8).
+// Два бинарных дерева считаются листоподобными, если их последовательность значений листьев одинакова.
+// Возвращает true тогда и только тогда, когда два заданных дерева с головными узлами root1 и root2 
+// являются листоподобными.
+// 872
+
+
+function TreeNode(val, left, right) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : left)
+    this.right = (right === undefined ? null : right)
+}
+// // Создаем узлы
+// const root = new TreeNode(3);
+// const node1 = new TreeNode(9);
+// const node2 = new TreeNode(20);
+// const node3 = new TreeNode(null);
+// const node4 = new TreeNode(null);
+// const node5 = new TreeNode(15);
+// const node6 = new TreeNode(7);
+// // Связываем узлы
+// root.left = node1;
+// root.right = node2;
+// node1.left = node3;
+// node1.right = node4;
+// node2.left = node5;
+// node2.right = node6;
+
+
+// Создаем узлы
+const root = new TreeNode(3);
+const node1 = new TreeNode(5);
+const node2 = new TreeNode(1);
+const node3 = new TreeNode(6);
+const node4 = new TreeNode(2);
+const node5 = new TreeNode(9);
+const node6 = new TreeNode(8);
+const node7 = new TreeNode(null);
+const node8 = new TreeNode(null);
+const node9 = new TreeNode(7);
+const node10 = new TreeNode(4);
+
+// Связываем узлы
+root.left = node1;
+root.right = node2;
+node1.left = node3;
+node1.right = node4;
+node2.left = node5;
+node2.right = node6;
+node3.left = node7;
+node3.right = node8;
+node4.left = node9;
+node4.right = node10;
+
+var leafSimilar = function (root1, root2) {
+
+
+
+};
+
+
+console.log(leafSimilar(root1, root2)) 
