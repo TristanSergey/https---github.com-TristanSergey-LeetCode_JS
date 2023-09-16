9c3b36dcfae361e791c2c0d2ed29fa89c40cdcf0
TristanSergey <tristan.sergey@gmail.com>
2023-09-16 13:22:33 +0300

    16092023

--- task_300/task_250/task_246_h_25.js
+++ task_300/task_250/task_246_h_25.js
@@ -5,3 +5,45 @@
 // какие они есть.
 // Вы не можете изменять значения в узлах списка, можно изменять только сами узлы.
 // 25
+
+
+const util = require('util');
+util.inspect.defaultOptions.depth = null;
+
+
+function ListNode(val, next) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+}
+
+let head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5, new ListNode(6, new ListNode(7, new ListNode(8, new ListNode(9, null)))))))));
+k = 3;
+
+var reverseKGroup = function (head, k) {
+
+    if (!head || !head.next) return head;
+    let previousGroupNode = null;
+    let currentNode = head;
+
+    //while (currentNode && currentNode.next) {
+    while (currentNode !== null) {
+        let count = 1;
+        let previousNode = null;
+        while (count <= k) {
+            //console.log(currentNode.next)
+            const temp = currentNode.next;
+            //console.log(temp)
+            currentNode.next = previousNode;
+            previousNode = currentNode;
+            currentNode = temp;
+            count++
+        }
+        const tempGroup = previousNode;
+        previousGroupNode = previousNode;
+        //console.log(previousGroupNode)
+    }
+    return previousGroupNode;
+
+};
+
+console.log(reverseKGroup(head, k))
