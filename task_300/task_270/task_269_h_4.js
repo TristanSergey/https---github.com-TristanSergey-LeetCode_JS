99aca32e5b7fab56a3ec44af9f3706485a78611e
TristanSergey <tristan.sergey@gmail.com>
2023-09-21 13:16:47 +0300

    21092023

---
+++ task_300/task_270/task_269_h_4.js
@@ -0,0 +1,14 @@
+﻿// Учитывая два отсортированных массива nums1 и nums2 размера m и n соответственно, 
+// верните медиану двух отсортированных массивов.
+// Общая сложность времени выполнения должна составлять O(log(m + n)).
+// 4
+
+nums1 = [1, 3], nums2 = [2]
+nums1 = [1, 2], nums2 = [3, 4]
+
+var findMedianSortedArrays = function (nums1, nums2) {
+
+};
+
+
+console.log(findMedianSortedArrays(nums1, nums2))
